4d3aa68262bf6e0dba77de74fed1fc99d42aa490
VyperArtz <69080969+VyperArtz@users.noreply.github.com>
2021-01-21 23:20:55 +0300

    Update ready.js

--- olaylar/ready.js
+++ olaylar/ready.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
 const config = require('../config.js');
 module.exports = async client => {
-  client.user.setPresence({ activity: { type: "WATCHING", name: ` 🛠️ Şuan bakımda | 🎈 Beta | 💰 z!yardım`}, status: 'idle' })
+  client.user.setPresence({ activity: { type: "WATCHING", name: ` 🛠️ Şuan bakımda | 🎈 Beta`}, status: 'idle' })
 };
